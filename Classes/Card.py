7a68cf7f366687195cf039a2464bde902ecf1887
Eric Phoumthipphavong <ericp@pocketgems.com>
2014-08-15 17:26:01 -0700

    initialize all of the card objects to None so if they don't exist we don't show them

--- Classes/Card.py
+++ Classes/Card.py
@@ -5,6 +5,35 @@ class Card(object):
     
 	def __init__(self, data):
 		self.attrlist = []
+		
+		#initialize all of the attributes to None
+		self.name = None
+		
+		self.manaCost = None
+		self.cmc = None
+		self.colors = None
+		
+		self.type = None
+		self.supertypes = None
+		self.types = None
+		self.subtypes = None
+		
+		self.rarity = None
+		
+		self.text = None
+		
+		self.flavor = None
+		
+		self.artist = None
+		self.number = None
+		
+		self.power = None
+		self.toughness = None
+		
+		self.layout = None
+		self.multiverseid = None
+		self.imageName = None
+		
 		for k,v in data.iteritems():
 			new_v = clean_unicode(v)
 			setattr(self, k, new_v)
